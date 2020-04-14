be8e4aa6e4936c0b1d7992ac83eec9f90a66b16a
Jakub Pomykala <n0743900@my.ntu.ac.uk>
2020-04-14 15:48:44 +0100

    login auto run

--- SystemSoftware/src/Client.java
+++ SystemSoftware/src/Client.java
@@ -1,18 +1,35 @@
-import java.net.*; 
+import java.net.*;
+import java.io.*;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.util.Scanner;
 
-public class Client
+class Client
 {
-    public static void main (String[] arg)
+    public static void main (String args[]) throws UnknownHostException, IOException
     {
-        try
+        String host = "127.0.0.1";
+        int port = 3000;
+        try (Socket socket = new Socket(host, port))
         {
-        Socket s = new Socket("127.0.0.1",3000);
-        System.out.println("Connected!!");
-        
-        loginPage.main(arg);
-        
-        }catch(Exception e){}
-        
-    }
-    
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner scanner = new Scanner(System.in);
+            String line = null;
+            loginPage.main(args);
+            while (!"exit".equalsIgnoreCase(line))
+            {
+                line = scanner.nextLine();
+                out.println(line);
+                out.flush();
+                System.out.println("Server replied " + in.readLine());
+            }
+            scanner.close();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }      
 }
--- SystemSoftware/src/Server.java
+++ SystemSoftware/src/Server.java
@@ -1,15 +1,90 @@
 import java.net.*;
+import java.io.*;
 
 public class Server 
 {
-    public static void main(String arg[])
+    
+    public static void main(String args[]) throws IOException
     {
+        ServerSocket server = null;
         try {
-        ServerSocket server = new ServerSocket(3000);
-        Socket s = server.accept();
-        
-        System.out.println("Connected");
-    } catch (Exception e) {}
+            server = new ServerSocket(3000);
+            server.setReuseAddress(true);
+            System.out.println("Waiting for client"); //when the server is first ran, this message is printed.         
+            while(true)
+            {  
+                Socket client = server.accept();
+                System.out.println("New client connected " + client.getInetAddress());
+            }
+         } catch (IOException e)
+         {
+             e.printStackTrace();
+         }
+        finally 
+        {
+            if (server != null)
+            {
+                try
+                {
+                server.close();
+            }catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+        }
     }
 }
+}
+
+class ClientHandler implements Runnable 
+{
+    private final Socket clientSocket;
+    
+    public ClientHandler(Socket socket) 
+    {
+        this.clientSocket = socket;
+    }
+    
+    @Override
+    public void run()
+    {
+        PrintWriter out = null;
+        BufferedReader in = null;
+        
+        try 
+        {
+            out = new PrintWriter (clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null)
+            {
+                System.out.printf("Sent from the client; %s\n", line);
+                out.println(line);
+            }           
+        }
+                    catch (IOException e)
+                    {
+                    e.printStackTrace();
+                    }
+        finally
+        {
+            try
+            {
+                if (out != null)
+                {
+                    out.close();
+                }
+                if (in != null)
+                {
+                    in.close();
+                    clientSocket.close();
+                }   
+            }catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
 
