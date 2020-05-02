705e99af5fc7f7fdc0fcc22fd3156f750c331cd3
Jakub Pomykala <n0743900@my.ntu.ac.uk>
2020-05-02 23:44:33 +0100

    enter key function for login page

--- SystemSoftware/src/loginPage.java
+++ SystemSoftware/src/loginPage.java
@@ -1,5 +1,6 @@
 
 
+import java.awt.event.KeyEvent;
 import javax.swing.JOptionPane;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -30,6 +31,7 @@ public class loginPage extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 0, 233));
+        setPreferredSize(new java.awt.Dimension(350, 280));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         usernameBox.setText("Username");
@@ -41,6 +43,11 @@ public class loginPage extends javax.swing.JFrame {
         getContentPane().add(usernameBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(136, 96, 123, -1));
 
         passwordBox.setText("..............");
+        passwordBox.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                passwordBoxKeyPressed(evt);
+            }
+        });
         getContentPane().add(passwordBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 140, 130, -1));
 
         cancelBtn.setText("Cancel");
@@ -140,6 +147,55 @@ public class loginPage extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_loginBtn1ActionPerformed
 
+    private void passwordBoxKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_passwordBoxKeyPressed
+         if(evt.getKeyCode() == KeyEvent.VK_ENTER){ 
+              if(passwordBox.getPassword().length!=0)  //if password not empty checks file txt
+            {
+                String csvFile = (System.getProperty("user.dir") + "/users.txt");
+                String line = "";
+                String cvsSplitBy = ",";
+                String passText = new String(passwordBox.getPassword());
+                String userText = new String(usernameBox.getText());
+                boolean found = false;
+                try (BufferedReader csvReader = new BufferedReader(new FileReader(csvFile)))
+                {
+                    while ((line = csvReader.readLine()) != null)
+                    {
+                        String[] user = line.split(cvsSplitBy);
+                       
+                       
+                        if(user[0].equals(userText) && user[1].equals(passText))
+                        {
+                            SystemSoftwareForm.runner(null);
+                            this.dispose();
+                            found = true;
+                            break;
+     
+                        }
+                    }
+                    if(!found)
+                    {
+                        JOptionPane.showMessageDialog(null, "ERROR : Invalid credentials! ");
+                        passwordBox.setText("");
+                        usernameBox.setText("");
+                    }
+
+                    
+               
+                } catch (IOException e) {        
+                    e.printStackTrace();
+                }
+               
+               
+            }
+            else
+            {
+               JOptionPane.showMessageDialog(null, "ERROR : Password empty! ");
+            }
+         }
+    }//GEN-LAST:event_passwordBoxKeyPressed
+
+    
     /**
      * @param args the command line arguments
      */
